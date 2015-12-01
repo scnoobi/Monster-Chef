c673d369901123e1ce35bf59e33a592a6245588b
sharkspirit <Antonio.m.pinto@ist.utl.pt>
2015-12-01 01:16:05 +0000

    tired and depressed

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
+    Vector3 originPos;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        originPos = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if(transform.position.y == originPos.y)
+        {
+            Activate();
+        }
 	}
+
+    void Activate()
+    {
+
+    }
 }
